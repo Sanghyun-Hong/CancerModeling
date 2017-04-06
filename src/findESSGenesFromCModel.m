7fc77ed0d8372c5e93fff643f6a8c4b05bbba2b2
Kyungjun Lee <kyungjun.kj.lee@gmail.com>
2017-04-05 23:50:45 -0400

    Implemented a function to find essential genes from cancer model

---
+++ src/findESSGenesFromCModel.m
@@ -0,0 +1,14 @@
+function [ essGenes ] = findESSGenesFromCModel( cmodel, threshold, clRatio )
+% cmodel: Cacner model (for now, assuming ESSbrca.mat)
+% threshold: threhsold to determine essential genes
+%            generally less than -0.5 will be ESS genes
+% clRatio: the ratio of essential ones through each gene's celllines to
+% determine whether that gene is considered as essential gene
+% essGenes: output --- a list of indexes of essential genes
+
+[geneNum, celllines] = size(cmodel.ESS);
+
+essGenes = cmodel.genes_ess(sum((cmodel.ESS < threshold) == 1, 2) >= celllines * clRatio);
+
+end
+
