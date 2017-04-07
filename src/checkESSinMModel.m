79e9656d7094a1b1595ab9456cb18cdcf491f741
Kyungjun Lee <kyungjun.kj.lee@gmail.com>
2017-04-07 15:38:48 -0400

    added knockingout a reaction function

--- src/checkESSinMModel.m
+++ src/checkESSinMModel.m
@@ -1,19 +1,47 @@
-function [ ess ] = checkESSinMModel( mmodel, essGenesNames )
+function [ essGeneIndexes, sol ] = checkESSinMModel( mmodel, method, essGenesNames )
 % Check the essentiality of a metabolic model with essential genes extracted from
 % cacner model
-% mmodel : metabolic model
+% mmodel: metabolic model
+% method: either FBA or MOMA
 % essGenesNames: a cell array of the name of essential genes from a cancer model
 % ess: output --- essentiality
 
+usingMOMA = -1;
+if strcmp(method, 'MOMA')
+  usingMOMA = 1;
+elseif strcmp(method, 'FBA')
+  usingMOMA = 0;
+else
+  % exit the program
+  fprintf('Usage: either MOMA or FBA needs to be specified in the parameter\n');
+  return
+end
+
 % Initialize variables
 geneNames = model.genes_unique_names;
-geneMap = model.genes_unique_map;
-rxnMat = model.rxnGeneMat;
 % Get an array of the index of essential genes
 essGeneIndexes = find(ismember(geneNames, essGenesNames));
-% TODO: Get an array of the index of associated reaction
-% rxnIndexes = rxnMat(:, essGeneIndexes); % this code is not working
 
-ess = None;
+% Knockout a reaction associated to each reaction in the array
+sol = zeros(length(essGeneIndexes));
+output = zeros(length(essGeneIndexes));
+for i = 1:length(essGeneIndexes)
+  modelC = mmodel;
+  gi = essGeneIndexes(i);
+  r = find(mmodel.rxnGeneMat(:,gi));
+  modelC.ub(r) = 0;
+  modelC.lb(r) = 0;
+  if usingMOMA == 1
+    % MOMA
+    output(i) = MOMA(mmodel, modelC);
+    sol(i) = output(i).f;
+  elseif not(usingMOMA)
+    % FBA
+    output(i) = optimizeCbModel(modelC);
+    sol(i) = output(i).f;
+  else
+    % should not reach here
+  end
+end
 
 end
