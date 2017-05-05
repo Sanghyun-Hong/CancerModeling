599220c3deed8eda18072aac6631c2702fead53f
Kyungjun Lee <kyungjun.kj.lee@gmail.com>
2017-05-05 01:22:37 -0400

    fixed bugs and implemented a function to get essential genes from metabolic model

---
+++ src/getESSGenesFromMModel.m
@@ -0,0 +1,77 @@
+function [ essGenes ] = getESSGenesFromMModel( mmodel, method, bmDropRatio )
+
+usingMOMA = -1;
+if strcmp(method, 'MOMA')
+  usingMOMA = 1;
+elseif strcmp(method, 'FBA')
+  usingMOMA = 0;
+else
+  % exit the program
+  fprintf('Usage: either MOMA or FBA needs to be specified in the parameter\n');
+  return
+end
+
+% get a base biomass value
+if usingMOMA == 1
+  [output, outputWT] = MOMA(mmodel, mmodel);
+  optSol= outputWT.f;
+else
+  output = optimizeCbModel(mmodel);
+  optSol = output.f;
+end
+
+for i = 1:length(mmodel.genes)
+  modelC = mmodel;
+  geneExp = sprintf('x(%d)', i);
+  rxns = find(~cellfun(@isempty, strfind(mmodel.rules, geneExp)) > 0);
+  
+  x = ones(size(mmodel.genes));
+  x(i) = 0;
+  
+  for j = 1:length(rxns)
+    rxnId = rxns(j);
+    if(~eval(mmodel.rules{rxnId}))
+      % knock-out
+      modelC.ub(rxnId) = 0;
+      modelC.lb(rxnId) = 0;
+    end
+  end
+  
+  if usingMOMA == 1
+    [output, outputWT] = MOMA(mmodel, modelC);
+    sol(i) = output.f;
+  else
+    FBAsol(i) = optimizeCbModel(modelC);
+    sol(i) = FBAsol(i).f;
+  end
+end
+
+% for i = 1:length(mmodel.genes)
+%   modelC = mmodel;
+%   inds = ones(size(mmodel.genes));
+%   inds(i) = 0;
+%   for j = 1:length(mmodel.rxns)
+%     vv(j) = evalExpRule2(model.rules{j}, inds);
+%   end
+%   modelC.ub(vv==0) = 0;
+%   modelC.lb(vv==0) = 0;
+%     
+%   if usingMOMA == 1
+%     [output, outputWT] = MOMA(mmodel, modelC);
+%     sol(i) = output.f;
+%   else
+%     FBAsol(i) = optimizeCbModel(modelC);
+%     sol(i) = FBAsol(i).f;
+%   end
+% end
+
+essGenesIndexes = find(sol < optSol * (1 - bmDropRatio));
+
+for i = 1:length(essGenesIndexes)
+  gi = essGenesIndexes(i);
+  % store a name of each essential gene
+  essGenes(i) = mmodel.genes_unique_names(mmodel.genes_unique_map(gi));
+end
+
+end
+
--- src/projscript.m
+++ src/projscript.m
@@ -1,5 +1,5 @@
 % prefix direcotry for the datasets folder
-datasets_dir = '/Users/jun/workspace/CancerModeling/datasets/';
+datasets_dir = '';
 
 % load ESSbrca.mat
 load(strcat(datasets_dir, 'ESSbrca.mat'));
@@ -12,20 +12,42 @@ load(strcat(datasets_dir, 'recon2.mat'));
 cdata = ESSbrca;
 r1model = recon1;
 %r2model = recon2;
+method = 'FBA';
 threshold = -0.5;
 cellineRatio = 0.9;
-method = 'FBA';
+bmDropRatio = 0.5;
+
+% name a csv file
+deli = '_';
+ext = '.csv';
+csvfile = char(strcat(method, deli, string(threshold), deli, ...
+             string(cellineRatio), deli, string(bmDropRatio), ext));
+           
+% open a csv file to write the method
+fid = fopen(csvfile, 'w');
+% first write parameters in the first column
+fprintf(fid, '%s, %f, %f, %f\n', method, threshold, cellineRatio, bmDropRatio);
 
 % get HER2 model
 [her2data, others] = getHER2fromCancerData(cdata);
 
 % get essential genes from HER2 model
 essGenes = findESSGenesFromCancerData(cdata, threshold, cellineRatio);
+% write a list of essential genes from HER cancer data into csv file
+toCSV = essGenes';
+fprintf(fid, '[ESSgenes of HER2],');
+fprintf(fid, '%s,', toCSV{1:end-1});
+fprintf(fid, '%s\n', toCSV{end});
 
-% check if the genes are essential in a metabolic model
-%[r1essGeneIndexes, r1Sol, r1optSol] = checkESSinMModel(r1model, method, essGenes);
-%[r2essGeneIndexes, r2Sol, r2optSol] = checkESSinMModel(r2model, method, essGenes);
+% get essential genes of a metabolic model
+essGenesOfModel = getESSGenesFromMModel(r1model, method, bmDropRatio);
+% write a list of essential genes from default metabolic model into csv file
+toCSV = essGenesOfModel';
+fprintf(fid, '[ESSgenes of RECON1],');
+fprintf(fid, '%s,', toCSV{1:end-1});
+fprintf(fid, '%s\n', toCSV{end});
 
+        
 % TODO: first calculate the base accuracy (by Mr. Hong)
 
 
@@ -37,12 +59,23 @@ genesToRem = findGenesToRemove(her2data, others, true);
 % find rxns from genes
 rxnsToRem = findRxnsWithGenesFromMModel(r1model, genesToRem);
 
-for i = 1:length(rxnToRem)
+for i = 1:length(rxnsToRem)
   mmodel = r1model;
   % modify a model by removing each reaction from the list
-  rxnName = mmodel.rxns(rxnToRem(i));
+  rxnName = mmodel.rxns(rxnsToRem(i));
   mmodel = removeRxns(mmodel, rxnName);
+  
+  % get essential genes of a MODIFIED metabolic model
+  essGenesOfModel = getESSGenesFromMModel(mmodel, method, bmDropRatio);
+  % write a list of essential genes from default metabolic model into csv file
+  toCSV = essGenesOfModel';
+  fprintf(fid, '[%s],', char(rxnName));
+  fprintf(fid, '%s,', toCSV{1:end-1});
+  fprintf(fid, '%s\n', toCSV{end});
 
   % TODO: calculate the accuracy of the modified model (by Mr. Hong)
   
 end
+
+fclose(fid);
+
