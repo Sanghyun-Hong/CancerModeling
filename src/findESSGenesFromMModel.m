715ce3815d32d1319979aeff480302065cc6a6ec
Kyungjun Lee <kyungjun.kj.lee@gmail.com>
2017-05-12 09:23:03 -0400

    modified to use rxnGeneMat to find associated reactions

--- src/findESSGenesFromMModel.m
+++ src/findESSGenesFromMModel.m
@@ -7,20 +7,25 @@ optBM = optSol.f;
 % 
 for i = 1:length(model.genes)
   modelC = model;
-  inds = ones(size(model.genes));
-  inds(i) = 0;
-  for j = 1:length(model.rxns)
-      vv(j) = evalExpRule2(model.rules{j}, inds);
-  end
-  modelC.ub(vv==0) = 0;
-  modelC.lb(vv==0) = 0;
+  %NOTE: not working as expected
+%   inds = ones(size(model.genes));
+%   inds(i) = 0;
+%   for j = 1:length(model.rxns)
+%       vv(j) = evalExpRule2(model.rules{j}, inds);
+%   end
+%   modelC.ub(vv==0) = 0;
+%   modelC.lb(vv==0) = 0;
+  % just use the rxnGeneMat for now
+  r = find(model.rxnGeneMat(:,i));
+  modelC.ub(r) = 0;
+  modelC.lb(r) = 0;
   FBAsol(i) = optimizeCbModel(modelC);
   sol(i) = FBAsol(i).f;
 end
 
 
-essGeneidx = unique(model.genes_unique_map(find(sol < optBM * (1 - bmDropRatio))));
-essGenes = model.genes_unique_names(essGeneidx);
+essGeneIdx = unique(model.genes_unique_map(find(sol < (optBM * (1 - bmDropRatio)))));
+essGenes = model.genes_unique_names(essGeneIdx);
 
 end
 
